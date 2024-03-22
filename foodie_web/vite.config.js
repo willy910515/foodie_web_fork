d46384054428c1cd47087aae7e6ade3cfa681a34
gary88888888 <a88386013@gmail.com>
2024-03-22 14:27:03 +0800

    add icon

--- foodie_web/vite.config.js
+++ foodie_web/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {  //服务器接口路径地址，根据路径设置
-        target: "http://114.32.9.225:3030",  //你的服务器地址
+        target: "http://localhost:3030",  //你的服务器地址
         changeOrigin: true,  // 允许跨域
         rewrite: (path) => path.replace(/^\/api/, '/api'), // 重写路径把路径变成空字符，（我理解的意思是：baseURL: '/neimenmenhuan/' 配置了，子接口 url: 'neimenmenhuan/settings/qx/user' 也可以正常访问，但是理解的很粗略，希望得到一个透彻的答案；
       },
